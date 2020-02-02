a7f3ef6d95aa55da766d9c99f567fa0a7c8db6bb
Francis Li <mail@francisli.com>
2020-02-01 16:12:58 -0800

    Add webextension polyfill for use in Chrome, remove console.log output

--- mobilize.js
+++ mobilize.js
@@ -1,5 +1,4 @@
 browser.runtime.sendMessage({}).then(function(data) {
-  console.log("response received:", data);
   for (let name of ["firstName", "lastName", "email", "phone", "zip"]) {
     const input = document.querySelector(`input[name="${name}"]`);
     if (input) {
