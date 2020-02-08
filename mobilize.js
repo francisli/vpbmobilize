96a83566ccf3dc5a7b872d946cb522c826e11fde
Francis Li <mail@francisli.com>
2020-02-07 18:43:39 -0800

    Make sure React form component updated

--- mobilize.js
+++ mobilize.js
@@ -1,8 +1,22 @@
+function setValue(input, newValue) {
+  let lastValue = input.value;
+  input.value = newValue;
+  let event = new Event('input', { bubbles: true });
+  // hack React15
+  event.simulated = true;
+  // hack React16
+  let tracker = input._valueTracker;
+  if (tracker) {
+    tracker.setValue(lastValue);
+  }
+  input.dispatchEvent(event);
+}
+
 browser.runtime.sendMessage({}).then(function(data) {
   for (let name of ["firstName", "lastName", "email", "phone", "zip"]) {
     const input = document.querySelector(`input[name="${name}"]`);
     if (input) {
-      input.value = data[name];
+      setValue(input, data[name]);
     }
   }
 });
